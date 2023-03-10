51997790b778c263a50b3802500149d5bceb07e8
Arhizmei <arhizmei@gmail.com>
2023-03-10 10:39:03 +0200

    1.2

---
+++ app/src/main/java/Constant/constants.kt
@@ -0,0 +1,9 @@
+package Constant
+
+object constants {
+
+    const val admin = "zmei"
+    const val user = "andrey"
+    const val admin_parol = "123456"
+    const val user_parol = "654321"
+}
--- app/src/main/java/com/zmei/secondactivity2/MainActivity.kt
+++ app/src/main/java/com/zmei/secondactivity2/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.zmei.secondactivity2
 
+import Constant.constants
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.SyncStateContract.Constants
 import com.zmei.secondactivity2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,23 @@ class MainActivity : AppCompatActivity() {
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
 
+    bindingClass.button1.setOnClickListener {
 
+    var  account = bindingClass.ed1.text.toString()
+    when (account){
+
+        constants.admin-> {
+            var parol = bindingClass.ed2.text.toString()
+            if (constants.admin_parol == parol)
+            {
+
+                
+            }
+            else bindingClass.textView1.text = "Wrong password"}
+
+        else -> {bindingClass.textView1.text = "Wrong password"}
+    }
+
+    }
     }
 }
