d9551c9a2a8f553e037d45b09386b70d7b61f3f9
Arhizmei <arhizmei@gmail.com>
2023-03-10 09:44:57 +0200

    1.0

--- app/src/main/java/com/zmei/secondactivity2/MainActivity.kt
+++ app/src/main/java/com/zmei/secondactivity2/MainActivity.kt
@@ -2,10 +2,16 @@ package com.zmei.secondactivity2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zmei.secondactivity2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bindingClass : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+
+
     }
 }
---
+++ app/src/main/java/com/zmei/secondactivity2/SecondActivity.kt
@@ -0,0 +1,4 @@
+package com.zmei.secondactivity2
+
+class SecondActivity {
+}
